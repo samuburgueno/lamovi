3fbd15f1b491367e6a452b202ac4d06efb36a5d0
sburgue0 <sburgue0@rosario.gov.ar>
2019-05-22 11:43:21 -0300

    Updates

---
+++ precache-manifest.149be6c1e74130ce880f71951850d4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc3ea4d8e351027f1111adcc8dd9f21",
+    "url": "/lamovi/index.html"
+  },
+  {
+    "revision": "7a6a45a188ba17020d4e",
+    "url": "/lamovi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "01d5a8e197505915d808",
+    "url": "/lamovi/static/js/2.ede10acd.chunk.js"
+  },
+  {
+    "revision": "7a6a45a188ba17020d4e",
+    "url": "/lamovi/static/js/main.c0262520.chunk.js"
+  },
+  {
+    "revision": "7d755f9f3d03660e2eab",
+    "url": "/lamovi/static/js/runtime~main.50993654.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lamovi/static/media/logo.5d5d9eef.svg"
+  }
+]);
